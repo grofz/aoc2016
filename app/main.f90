12f01c195b5abaf09dce8a7e5a2034ed87cfc67f
Zdenek Grof <grofz@vscht.cz>
2022-11-19 09:33:20 +0100

    Day 4 solved.

--- app/main.f90
+++ app/main.f90
@@ -3,11 +3,13 @@ program main
   use day1601_mod, only: day1601
   use day1602_mod, only: day1602
   use day1603_mod, only: day1603
+  use day1604_mod, only: day1604
   implicit none
 
   !call day1601('./inp/01/input.txt')
   !call day1602('./inp/02/input.txt')
   !call day1602('./inp/02/test.txt')
-  call day1603('./inp/03/input.txt')
+  !call day1603('./inp/03/input.txt')
+  call day1604('./inp/04/input.txt')
   !call day1610('./inp/input.txt')
 end program main
